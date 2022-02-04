7fec081ee4661dab2e3f9423a3bfc859c73e777d
Mary <maria.hej253@gmail.com>
2022-02-04 13:42:15 +0200

    rough setup of main

---
+++ main.py
@@ -0,0 +1,20 @@
+# https://sourceforge.net/directory/os:windows/
+
+def scrape():
+    pass
+
+
+def rename_sha1(name):
+    pass
+
+
+def seven_zip(files):
+    pass
+
+def main():
+    scrape()
+    rename_sha1(name)
+    seven_zip(files)
+
+if __name__ == "__main__":
+    main()
