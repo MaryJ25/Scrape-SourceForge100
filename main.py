5595bbf0aa5b118b0b708f8178a5bfba78e57011
Mary <maria.hej253@gmail.com>
2022-02-08 00:48:50 +0200

    it gets links and downloads files, but not all of them

--- main.py
+++ main.py
@@ -1,52 +1,76 @@
-from variables import FOLDER
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
-from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.common.by import By
 import sys
 import os
 from time import sleep
+
 
 # driver.quit()
 
 
 def browser_setup():
-    path = os.path.join(os.environ['USERPROFILE'], FOLDER, r'geckodriver-v0.30.0-win64\geckodriver.exe')
-    print(path)
+    download_dir = os.path.join((os.environ['USERPROFILE']), r'Desktop\Downloaded-SF\\')
+    if not os.path.exists(download_dir):
+        os.makedirs(download_dir)
+    fp = webdriver.FirefoxProfile()
+    fp.set_preference("browser.download.folderList", 2)
+    fp.set_preference("browser.download.dir", download_dir)
+    fp.set_preference("browser.download.manager.showWhenStarting", False)
+    fp.set_preference("browser.helperApps.neverAsk.saveToDisk", "application" "application/x-7z-compressed"
+                                                                "application/vnd.microsoft.portable-executable"
+                                                                "application/x-msi" "application/x-dosexec"
+                                                                "application/octet-stream" "application/x-gzip")
+
     options = FirefoxOptions()
-    # options.headless = True
-    # caps = DesiredCapabilities().FIREFOX
-    # caps["marionette"] = True
-    driver = webdriver.Firefox(executable_path=path, options=options)
+    #options.headless = True
+    caps = DesiredCapabilities().FIREFOX
+    #caps["marionette"] = True
+    driver = webdriver.Firefox(firefox_profile=fp, capabilities=caps, executable_path=GeckoDriverManager().install(), options=options)
     return driver
 
 
-def open_page(driver, url):
-    main_page = "https://sourceforge.net/"
-    # adjusts the link to go to the correct page
-    browser = driver.get(main_page + url)
-    return browser
+def get_project_links(driver, quantity):
+    url = fr"https://sourceforge.net/directory/os:windows/"
+    driver.get(url)
+    cookie = driver.find_element(By.CLASS_NAME, "cmpboxbtn")
+    cookie.click()
+    only_links = set()
+    while len(only_links) < quantity+10:
+        for link in driver.find_elements(By.CLASS_NAME, "see-project"):
+            only_links.add(link.get_attribute("href"))
+        next_page = driver.find_element(By.CSS_SELECTOR, "[aria-label='Next page']")
+        next_page.click()
+    only_links = list(only_links)
+    return only_links[:quantity]
 
 
-def get_project_links(quantity):
-    pass
-
-
-def download(links):
-    pass
+def download(links, driver):
+    for link in links:
+        driver.get(link)
+        download_file = driver.find_element(By.CLASS_NAME, "download")
+        download_file.click()
+        sleep(7)
+        break
 
 
 def scrape():
     driver = browser_setup()
-    open_page(driver, r"directory/os:windows/?page=2")
-    sleep(5)
+    try:
+       links = get_project_links(driver, 100)
+       download(links, driver)
+    except Exception as e:
+        print(e)
+        driver.quit()
+    sleep(60)
     driver.quit()
-    # links = get_project_links(100)
-    # download(links)
 
 
 def rename_sha1(name):
+    extensions = ["acm", "ax", "cpl", "dll", "drv", "efi", "exe", "mui", "ocx", "scr", "sys", "tsp"]
     pass
 
 
